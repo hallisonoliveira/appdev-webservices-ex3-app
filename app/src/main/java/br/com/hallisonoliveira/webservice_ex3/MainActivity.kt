5b1f2785d604106ebd1e4bae414a6bc2735b192a
Hallison Oliveira <hallisoncwb@gmail.com>
2020-05-29 23:53:29 -0300

    Create project

---
+++ app/src/main/java/br/com/hallisonoliveira/webservice_ex3/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.hallisonoliveira.webservice_ex3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
